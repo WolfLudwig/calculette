1e5e54c7acaac1afcfaa35b0faf6e85747555c77
WolfLudwig <[wolf_ludwig@outlook.com]>
2019-07-25 14:49:53 +0200

    Vers l'infini et l'au delà

--- js/calcu.js
+++ js/calcu.js
@@ -1,16 +1,20 @@
-console.log(eval(12+2-1));
+// console.log(eval(12+2-1));
+let input ="";
 
-let button = document.getElementsByClassName("button")
-console.log(button);
-button.onclick = console.log(button.value);
-let input = "";
+let buttons = document.getElementsByClassName("button");
+console.log(buttons[5].value);
 
-function showvalue(button){
-    input = input + button.value;
-
-    document.getElementById("output").innerHTML = input;
+for (let i = 0; i < buttons.length; i++){
+    // console.log(buttons[i].value);
+    buttons[i].addEventListener("click", showvalue);
 }
 
-function domaths(){
-    
-}
+let rset = document.getElementById("reset");
+rset.addEventListener("click", reset);
+
+let rmove = document.getElementById("remove");
+rmove.addEventListener("click", remove);
+
+let equal = document.getElementById("result")
+equal.addEventListener("click", domaths)
+
