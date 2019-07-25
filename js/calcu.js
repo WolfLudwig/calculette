a2442bf493ab00a25524615ce8cb1a81c9d3587b
WolfLudwig <[wolf_ludwig@outlook.com]>
2019-07-25 15:51:26 +0200

    J'me sens comme une bamba triste

--- js/calcu.js
+++ js/calcu.js
@@ -2,7 +2,7 @@
 let input ="";
 
 let buttons = document.getElementsByClassName("button");
-console.log(buttons[5].value);
+// console.log(buttons[5].value);
 
 for (let i = 0; i < buttons.length; i++){
     // console.log(buttons[i].value);
@@ -18,3 +18,23 @@ rmove.addEventListener("click", remove);
 let equal = document.getElementById("result")
 equal.addEventListener("click", domaths)
 
+function showvalue(){
+    input = input + this.value;
+
+    document.getElementById("output").innerHTML = input;
+}
+
+function domaths(){
+    document.getElementById("output").innerHTML = eval(input);
+    input = ""
+}
+
+function reset(){
+    document.getElementById("output").innerHTML = "Un autre calcul?"
+    input = ""
+}
+
+function remove(){
+    input = input.replace(input.slice(-1), "")
+    document.getElementById("output").innerHTML = input;
+}
