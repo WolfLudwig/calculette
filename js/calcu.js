9779dde923fc3035dfcd7fe77a8952eaca4f5059
WolfLudwig <[wolf_ludwig@outlook.com]>
2019-07-25 12:42:08 +0200

    Pas de pitié pour les croissants

--- js/calcu.js
+++ js/calcu.js
@@ -1,7 +1,16 @@
 console.log(eval(12+2-1));
 
-let input = " ";
+let button = document.getElementsByClassName("button")
+console.log(button);
+button.onclick = console.log(button.value);
+let input = "";
 
 function showvalue(button){
+    input = input + button.value;
+
+    document.getElementById("output").innerHTML = input;
+}
+
+function domaths(){
     
 }
